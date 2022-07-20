e32de27c3ae201e0ead58b55b58d73c3120e74e6
CarvalhoVincent <carvalho.vincent@hotmail.fr>
2022-07-20 17:48:16 +0200

    mobile header ok

---
+++ index.js
@@ -0,0 +1,24 @@
+// Toggle Menu //
+
+function displayMenu() {
+    const menu = document.getElementById("primary-navigation");
+    const state = document.querySelector(".mobile-nav-toggle");
+    const footer = document.querySelector("footer");
+    menu.classList.toggle("primary-navigation-toggle");
+
+    if(state.getAttribute("data-visible") === "false") {
+        menu.style = "box-shadow: 0px 0px 200px 100vh rgba(0,0,0,0.5);"
+        state.style = "background-image: url(./images/icon-close-menu.svg); position: fixed;";
+        state.setAttribute("data-visible", true);
+        state.setAttribute("aria-expanded", true);
+        footer.style.background = "#000000";
+        footer.style.opacity = ".5";
+    } else {
+        menu.style = "box-shadow: none;"
+        state.style = "background-image: url(./images/icon-hamburger.svg); position: absolute;";
+        state.setAttribute("data-visible", false);
+        state.setAttribute("aria-expanded", false);
+        footer.style.background = "";
+        footer.style.opacity = "";
+    }
+}
